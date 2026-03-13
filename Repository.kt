ad40b49d1312738725e2b7c0d178a228d9daac51
Raghu2424 <raghavendrakshetri@gmail.com>
2026-03-13 19:32:54 +0530

    Add Repository class for weather API calls

--- Repository.kt
+++ Repository.kt
@@ -1,1 +1,10 @@
+package com.example.weatherapp
 
+class Repository {
+
+    private val api = ApiClient.retrofit.create(ApiService::class.java)
+
+    suspend fun getWeather(city: String, apiKey: String): WeatherResponse {
+        return api.getWeather(city, apiKey)
+    }
+}
