074ee859811ae79fa66e43de3c20d35acf4436eb
Raghu2424 <raghavendrakshetri@gmail.com>
2026-03-13 19:33:56 +0530

    Implement WeatherViewModel to fetch weather data

--- ViewModel.kt
+++ ViewModel.kt
@@ -1,1 +1,15 @@
+package com.example.weatherapp
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.Dispatchers
+
+class WeatherViewModel : ViewModel() {
+
+    private val repository = Repository()
+
+    fun getWeather(city: String, apiKey: String) = liveData(Dispatchers.IO) {
+        val data = repository.getWeather(city, apiKey)
+        emit(data)
+    }
+}
